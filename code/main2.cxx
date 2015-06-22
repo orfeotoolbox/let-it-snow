be1eb591091b641b4f281e002305c6d9fac74984
Manuel Grizonnet <manuel.grizonnet@gmail.com>
2015-06-22 14:51:03 +0200

    manage 2 histograms to get zs

--- code/main2.cxx
+++ code/main2.cxx
@@ -39,8 +39,9 @@ typedef otb::StreamingMinMaxVectorImageFilter<VectorImageType>     StreamingMinM
 int main(int argc, char * argv [])
 {
   const char * infname = argv[1];
-  const char * inmaskfname = argv[2];
-  const int maskVal = atoi(argv[3]);
+  const char * inmasksnowfname = argv[2];
+  const char * inmaskcloudfname = argv[3];
+  //const int maskVal = atoi(argv[4]);
 
   VectorReaderType::Pointer reader = VectorReaderType::New();
   reader->SetFileName(infname);
@@ -48,8 +49,11 @@ int main(int argc, char * argv [])
   reader->UpdateOutputInformation();
   const unsigned int nbComp = reader->GetOutput()->GetNumberOfComponentsPerPixel();
 
-  MaskReaderType::Pointer readerm = MaskReaderType::New();
-  readerm->SetFileName(inmaskfname);
+  MaskReaderType::Pointer reader_snow = MaskReaderType::New();
+  reader_snow->SetFileName(inmasksnowfname);
+
+  MaskReaderType::Pointer reader_cloud = MaskReaderType::New();
+  reader_cloud->SetFileName(inmaskcloudfname);
 
   // Instantiating object
   StreamingMinMaxImageFilterType::Pointer filter = StreamingMinMaxImageFilterType::New();
@@ -64,39 +68,58 @@ int main(int argc, char * argv [])
   min=filter->GetMinimum();
   max=filter->GetMaximum();
 
-  SHVIFType::Pointer SHVIFFilter = SHVIFType::New();
+  SHVIFType::Pointer SHVIFFilter1 = SHVIFType::New();
+  SHVIFType::Pointer SHVIFFilter2 = SHVIFType::New();
 
-  SHVIFFilter->GetFilter()->SetInput(reader->GetOutput());
+  SHVIFFilter1->GetFilter()->SetInput(reader->GetOutput());
+  SHVIFFilter2->GetFilter()->SetInput(reader->GetOutput());
+  
   SHVIFType::FilterType::CountVectorType bins( nbComp );
 
   std::cout << "min " << filter->GetMinimum() << std::endl;
   std::cout << "max " << filter->GetMaximum() << std::endl;
 
   bins[0]=(max[0]-min[0]) / 100;
-  SHVIFFilter->GetFilter()->SetNumberOfBins( bins );
-
-  SHVIFFilter->GetFilter()->SetHistogramMin( min );
-  SHVIFFilter->GetFilter()->SetHistogramMax( max );
- 
-  //set mask
-  SHVIFFilter->SetMaskImage(readerm->GetOutput());
   
-  SHVIFFilter->SetMaskValue(maskVal);
+  SHVIFFilter1->GetFilter()->SetNumberOfBins( bins );
+  SHVIFFilter1->GetFilter()->SetHistogramMin( min );
+  SHVIFFilter1->GetFilter()->SetHistogramMax( max );
+ 
+  SHVIFFilter2->GetFilter()->SetNumberOfBins( bins );
+  SHVIFFilter2->GetFilter()->SetHistogramMin( min );
+  SHVIFFilter2->GetFilter()->SetHistogramMax( max );
 
-  SHVIFFilter->Update();
+  //set mask
+  SHVIFFilter1->SetMaskImage(reader_cloud->GetOutput());
+  SHVIFFilter1->SetMaskValue(0);
+  SHVIFFilter1->Update();
 
-  HistogramList::Pointer histograms = SHVIFFilter->GetHistogramList();
+  SHVIFFilter2->SetMaskImage(reader_snow->GetOutput());
+  SHVIFFilter2->SetMaskValue(1);
+  SHVIFFilter2->Update();
+
+  HistogramList::Pointer histograms1 = SHVIFFilter1->GetHistogramList();
+  HistogramList::Pointer histograms2 = SHVIFFilter2->GetHistogramList();
 
   unsigned int channel = 0;  // first channel
-  Histogram::Pointer histogram( histograms->GetNthElement( channel ) );
+  Histogram::Pointer histogram1( histograms1->GetNthElement( channel ) );
+  Histogram::Pointer histogram2( histograms2->GetNthElement( channel ) );
 
-  unsigned int histogramSize = histogram->Size();
+  unsigned int histogramSize1 = histogram1->Size();
 
-  std::cout << "Histogram size " << histogramSize << std::endl;
+  std::cout << "Histogram size " << histogramSize1 << std::endl;
 
-  for( unsigned int bin=0; bin < histogramSize; bin++ )
+  for( unsigned int bin=0; bin < histogramSize1; bin++ )
     {
-    std::cout << "Histogram frequency " << histogram->GetFrequency( bin, 0 ) << std::endl;
+    std::cout << "Histogram 1 frequency " << histogram1->GetFrequency( bin, 0 ) << std::endl;
+    std::cout << "Histogram 2 frequency " << histogram2->GetFrequency( bin, 0 ) << std::endl;
+    std::cout << std::endl;
+
+    if ((float) histogram2->GetFrequency( bin, 0 ) / (float) histogram1->GetFrequency( bin, 0 ) > 0.1)
+      {
+      std::cout << "Measurement vector at bin " << bin << " is "
+            << histogram1->GetMeasurementVector(bin-2) << std::endl;
+      }
     }
 
   return EXIT_SUCCESS;
