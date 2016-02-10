a8d6fe631c959f98fa89faff399f142133498e66
Tristan Klempka <tristan.klempka@cnes.fr>
2016-02-10 15:21:21 +0100

    ENH: add s2snow output tests and update paths

---
+++ test/test_json_builder.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+#Build a test Json
+#INPUTS
+#Config path
+#LIS data path
+#Output path
+
+configPath=$1
+dataPath=$2
+outputPath=$3
+
+#exit 1 on error
+set -e 
+
+# move to config path to build the json file.
+cd $configPath
+
+rm -f paramTest.json
+cp param_full_Take5_template.json param_test.json
+
+# modify only three parameters: image file, cloud file, dem file, output dir
+inputimage=$(find $dataPath -name *ORTHO_SURF_CORR_PENTE*.TIF)
+inputcloud=$(find $dataPath -name *NUA.TIF)
+inputdem=$(find $dataPath/SRTM/* -name *.tif)
+sed -i -e "s|inputimage|$inputimage|g" param_test.json
+sed -i -e "s|inputcloud|$inputcloud|g" param_test.json
+sed -i -e "s|inputdem|$inputdem|g" param_test.json
+sed -i -e "s|outputdir|$outputPath|g" param_test.json
+exit 0
+ 
+ 
