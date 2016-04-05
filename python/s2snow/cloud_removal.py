bc88303dda6184542ab3890c96a4983cf0b94f8a
Tristan Klempka <tristan.klempka@cnes.fr>
2016-04-05 15:27:29 +0200

    ENH: add json config for cloud_removal.py

--- python/s2snow/cloud_removal.py
+++ python/s2snow/cloud_removal.py
@@ -1,4 +1,4 @@
-import sys, numpy
+import sys, numpy, json, multiprocessing
 from scipy import ndimage
 from subprocess import call
 import os
@@ -8,117 +8,170 @@ import gdalconst
 
 def show_help():
 	print "This script is used to remove clouds from snow data"
-	print "Usage: TODO"
-        print "TODO"
-def main(argv):
-    m2_path = argv[1]
-    m1_path = argv[2]
-    t0_path = argv[3]
-    p1_path = argv[4]
-    p2_path = argv[5]
-    dem_path = argv[6]
-    metadata_path = argv[7]
-    output_path = argv[8]
-    
+	print "Usage: cloud_removal.py config.json"
+
+def step1(m1_path, t0_path, p1_path, output_path, ram):
     #S(y,x,t) = 1 if (S(y,x,t-1) = 1 and S(y,x,t+1) = 1) 
-    call(["otbcli_BandMath", "-il", m1_path, t0_path, p1_path, "-out", op.join(output_path, "cloud_removal_pass1.tif"), "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
+    call(["otbcli_BandMath","-ram", str(ram), "-il", m1_path, t0_path, p1_path, "-out", output_path, "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
+
+def step2(m2_path, m1_path, t0_path, p1_path, p2_path, output_path, ram):
     #S(y,x,t) = 1 if (S(y,x,t-2) = 1 and S(y,x,t+1) = 1)
-    call(["otbcli_BandMath", "-il", m2_path, op.join(output_path, "cloud_removal_pass1.tif"), p1_path, "-out", op.join(output_path, "cloud_removal_pass2.tif"), "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
-     #S(y,x,t) = 1 if (S(y,x,t-1) = 1 and S(y,x,t+2) = 1)
-    call(["otbcli_BandMath", "-il", m1_path, op.join(output_path, "cloud_removal_pass2.tif"), p2_path, "-out", op.join(output_path, "cloud_removal_pass3.tif"), "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
-    
+	call(["otbcli_BandMath","-ram", str(ram), "-il", m2_path, t0_path, p1_path, "-out", output_path, "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
+    #S(y,x,t) = 1 if (S(y,x,t-1) = 1 and S(y,x,t+2) = 1)
+	call(["otbcli_BandMath","-ram", str(ram), "-il", m1_path, output_path, p2_path, "-out", output_path, "-exp", "im2b1==100?100:(im1b1==100&&im3b1==100)?100:im2b1"])
+
+def step3(t0_path, dem_path, hs_min, hs_max, output_path, ram):
     #S(y,x,t) = 1 if (H(x,y) < Hsmin(t))
-    #TODO retreive ZS test if - 70% de nuages
-    ZS_min=2800
-    print ZS_min
-    call(["otbcli_BandMath", "-il", op.join(output_path, "cloud_removal_pass3.tif"), dem_path, "-out", op.join(output_path, "cloud_removal_pass4.tif"), "-exp", "im1b1==205?(im2b1<"+str(ZS_min)+"?255:im1b1):im1b1"])
+    call(["otbcli_BandMath","-ram", str(ram), "-il", t0_path, dem_path, "-out", output_path, "-exp", "im1b1==205?(im2b1<"+str(hs_min)+"?255:im1b1):im1b1"])
  
     #S(y,x,t) = 1 if (H(x,y) > Hsmax(t))
-    ZS_max = -1
-    print ZS_max
-    if(ZS_max != -1):
-            call(["otbcli_BandMath", "-il", op.join(output_path, "cloud_removal_pass4.tif"), dem_path, "-out", op.join(output_path, "cloud_removal_pass5.tif"), "-exp", "im1b1==205?(im2b1>"+str(ZS_max)+"?255:im1b1):im1b1"])
-    #Do nothing but create image still
-    else:
-            call(["otbcli_BandMath", "-il", op.join(output_path, "cloud_removal_pass4.tif"), dem_path, "-out", op.join(output_path, "cloud_removal_pass5.tif"), "-exp", "im1b1"])
-        
-    # four-pixels neighboring
-    dataset_pass5 = gdal.Open(op.join(output_path, "cloud_removal_pass5.tif"), gdalconst.GA_ReadOnly)
-    wide = dataset_pass5.RasterXSize
-    high = dataset_pass5.RasterYSize
-    band_pass5 = dataset_pass5.GetRasterBand(1)
-    array_pass5 = band_pass5.ReadAsArray(0, 0, wide, high)
-    
-    # Get west, north, east & south elements for [1:-1,1:-1] region of input array
-    W = array_pass5[1:-1,:-2]
-    N  = array_pass5[:-2,1:-1]
-    E = array_pass5[1:-1,2:]
-    S  = array_pass5[2:,1:-1]
+    call(["otbcli_BandMath","-ram", str(ram), "-il", output_path, dem_path, "-out", output_path, "-exp", "im1b1==205?(im2b1>"+str(hs_max)+"?255:im1b1):im1b1"])
 
-    # Check if all four arrays have 100 for that same element in that region
-    mask = (W == 100) & (N == 100) & (E == 100) & (S == 100) & (array_pass5[1:-1,1:-1] == 205)
+def step4(t0_path, output_path, window_size):
     
-    # Use the mask to set corresponding elements in a copy version as 100
-    array_pass6 = array_pass5.copy()
-    array_pass6[1:-1,1:-1][mask] = 100
-    
-    #create file
-    output_pass6 = gdal.GetDriverByName('GTiff').Create(op.join(output_path, "cloud_removal_pass6.tif"), wide, high, 1 ,gdal.GDT_Byte)
-    output_pass6.GetRasterBand(1).WriteArray(array_pass6)
-    
-    # georeference the image and set the projection
-    output_pass6.SetGeoTransform(dataset_pass5.GetGeoTransform())
-    output_pass6.SetProjection(dataset_pass5.GetProjection()) 
-    output_pass6 = None
-    
-    # S(y,x,t) = 1 if (S(y+k,x+k,t)(kc(-1,1)) = 1 and H(y+k,x+k)(kc(-1,1)) < H(y,x))
-    dataset_pass6 = gdal.Open(op.join(output_path, "cloud_removal_pass6.tif"), gdalconst.GA_ReadOnly)    
-    wide = dataset_pass6.RasterXSize
-    high = dataset_pass6.RasterYSize
-    band_pass6 = dataset_pass6.GetRasterBand(1)
-    array_pass6 = band_pass6.ReadAsArray(0, 0, wide, high)
-    
-    dataset_dem = gdal.Open(op.join(output_path, "srtm.tif"), gdalconst.GA_ReadOnly)
-    band_dem = dataset_dem.GetRasterBand(1)
-    array_dem = band_dem.ReadAsArray(0, 0, wide, high)
-    
-    # Get 8 neighboring pixels for raster and dem 
-    W = array_pass6[1:-1,:-2]
-    NW = array_pass6[:-2,:-2]
-    N  = array_pass6[:-2,1:-1]
-    NE = array_pass6[:-2,2:]
-    E = array_pass6[1:-1,2:]
-    SE = array_pass6[2:,2:]
-    S  = array_pass6[2:,1:-1]
-    SW = array_pass6[2:,:-2]
-    
-    Wdem = array_dem[1:-1,:-2]
-    NWdem = array_dem[:-2,:-2]
-    Ndem  = array_dem[:-2,1:-1]
-    NEdem = array_dem[:-2,2:]
-    Edem = array_dem[1:-1,2:]
-    SEdem = array_dem[2:,2:]
-    Sdem  = array_dem[2:,1:-1]
-    SWdem = array_dem[2:,:-2]
+	# four-pixels neighboring    
+	print "Starting step 4"
+	dataset = gdal.Open(t0_path, gdalconst.GA_ReadOnly)
+	wide = dataset.RasterXSize
+	high = dataset.RasterYSize
+	band = dataset.GetRasterBand(1)
+	array = band.ReadAsArray(0, 0, wide, high)
 
-    
-    mask1 =((((W == 100) & (array_dem[1:-1,1:-1] > Wdem)) | ((N == 100) & (array_dem[1:-1,1:-1] > Ndem)) | ((E == 100) & (array_dem[1:-1,1:-1] > Edem)) | ((S == 100) & (array_dem[1:-1,1:-1] > Sdem))  | ((NW == 100) & (array_dem[1:-1,1:-1] > NWdem)) | ((NE == 100) & (array_dem[1:-1,1:-1] > NEdem)) | ((SE == 100) & (array_dem[1:-1,1:-1] > SEdem)) | ((SW == 100) & (array_dem[1:-1,1:-1] > SWdem))) & (array_pass5[1:-1,1:-1] == 205))
-    
-    # Use the mask to set corresponding elements in a copy version as 100
-    array_pass7 = array_pass6.copy()
-    array_pass7[1:-1,1:-1][mask] = 100
+	# Get west, north, east & south elements for [1:-1,1:-1] region of input array
+	W = array[1:-1,:-2]
+	N  = array[:-2,1:-1]
+	E = array[1:-1,2:]
+	S  = array[2:,1:-1]
+	
+	# Check if all four arrays have 100 for that same element in that region
+	mask = (W == 100) & (N == 100) & (E == 100) & (S == 100) & (array[1:-1,1:-1] == 205)
+	
+	# Use the mask to set corresponding elements in a copy version as 100
+	array_out = array.copy()
+	array_out[1:-1,1:-1][mask] = 100
+	
+	#create file
+	output = gdal.GetDriverByName('GTiff').Create(output_path, wide, high, 1 ,gdal.GDT_Byte)
+	output.GetRasterBand(1).WriteArray(array_out)
+	
+	# georeference the image and set the projection
+	output.SetGeoTransform(dataset.GetGeoTransform())
+	output.SetProjection(dataset.GetProjection()) 
+	output = None
+	print "End of step 4"
 
-    #create file
-    output_pass7 = gdal.GetDriverByName('GTiff').Create(op.join(output_path, "cloud_removal_pass7.tif"), wide, high, 1 ,gdal.GDT_Byte)
-    output_pass7.GetRasterBand(1).WriteArray(array_pass7) 
-        
-    # georeference the image and set the projection
-    output_pass7.SetGeoTransform(dataset_pass6.GetGeoTransform())
-    output_pass7.SetProjection(dataset_pass6.GetProjection()) 
-    output_pass7 = None
+def step5(t0_path, dem_path, output_path, window_size):
+	# S(y,x,t) = 1 if (S(y+k,x+k,t)(kc(-1,1)) = 1 and H(y+k,x+k)(kc(-1,1)) < H(y,x))
+	print "Starting step 5"
+	dataset = gdal.Open(t0_path, gdalconst.GA_ReadOnly)    
+	wide = dataset.RasterXSize
+	high = dataset.RasterYSize
+	band = dataset.GetRasterBand(1)
+	array = band.ReadAsArray(0, 0, wide, high)
+
+	dataset_dem = gdal.Open(dem_path, gdalconst.GA_ReadOnly)
+	band_dem = dataset_dem.GetRasterBand(1)
+	array_dem = band_dem.ReadAsArray(0, 0, wide, high)
+	
+	# Get 8 neighboring pixels for raster and dem 
+	W = array[1:-1,:-2]
+	NW = array[:-2,:-2]
+	N  = array[:-2,1:-1]
+	NE = array[:-2,2:]
+	E = array[1:-1,2:]
+	SE = array[2:,2:]
+	S  = array[2:,1:-1]
+	SW = array[2:,:-2]
+	
+	Wdem = array_dem[1:-1,:-2]
+	NWdem = array_dem[:-2,:-2]
+	Ndem  = array_dem[:-2,1:-1]
+	NEdem = array_dem[:-2,2:]
+	Edem = array_dem[1:-1,2:]
+	SEdem = array_dem[2:,2:]
+	Sdem  = array_dem[2:,1:-1]
+	SWdem = array_dem[2:,:-2]
+
+	arrdem = array_dem[1:-1,1:-1]
+	mask =((((W == 100) & (arrdem > Wdem)) | ((N == 100) & (arrdem > Ndem)) | ((E == 100) & (arrdem > Edem)) | ((S == 100) & (arrdem > Sdem))  | ((NW == 100) & (arrdem > NWdem)) | ((NE == 100) & (arrdem > NEdem)) | ((SE == 100) & (arrdem > SEdem)) | ((SW == 100) & (arrdem > SWdem))) & (array[1:-1,1:-1] == 205))
+
+	# Use the mask to set corresponding elements in a copy version as 100
+	array_out = array.copy()
+	array_out[1:-1,1:-1][mask] = 100
+	
+	#create file
+	output = gdal.GetDriverByName('GTiff').Create(output_path, wide, high, 1 ,gdal.GDT_Byte)
+	output.GetRasterBand(1).WriteArray(array) 
+	
+	# georeference the image and set the projection
+	output.SetGeoTransform(dataset.GetGeoTransform())
+	output.SetProjection(dataset.GetProjection()) 
+	output = None
+	print "End of step 5"
+
+def main(argv):
+
+	json_file=argv[1]
+	with open(json_file) as json_data_file:
+		data = json.load(json_data_file)
+		
+	general=data["general"]
+	output_path=general.get("pout")
+	ram=general.get("ram", 512)
+
+	try:
+		nb_defaultThreads = multiprocessing.cpu_count()
+	except NotImplementedError:
+		print "Cannot get max number of CPU on the system. nbDefaultThreads set to 1."  
+		nb_defaultThreads = 1
+		
+	nb_threads=general.get("nb_threads", nb_defaultThreads)
+	os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"]=str(nb_threads)
+	
+	inputs=data["inputs"]    
+	m2_path=inputs.get("m2Path")
+	m1_path=inputs.get("m1Path")
+	t0_path=inputs.get("t0Path")
+	p1_path=inputs.get("p1Path")
+	p2_path=inputs.get("p2Path")
+	dem_path=inputs.get("demPath")
+
+	parameters=data["parameters"]
+	hs_min=parameters.get("hsMin")
+	hs_max=parameters.get("hsMax")
+	window_size=parameters.get("windowSize")
+	
+	steps=data["steps"]
+	s1=steps.get("s1", True)
+	s2=steps.get("s2", True)
+	s3=steps.get("s3", True)
+	s4=steps.get("s4", True)
+	s5=steps.get("s5", True)
+	
+	latest_file_path=t0_path
+	if s1:
+		temp_output_path=op.join(output_path, "cloud_removal_output_step1.tif")
+		step1(m1_path, latest_file_path, p1_path, temp_output_path, ram)
+		latest_file_path=temp_output_path
+	if s2:
+		temp_output_path=op.join(output_path, "cloud_removal_output_step2.tif")
+		step2(m2_path, m1_path, latest_file_path, p1_path, p2_path, temp_output_path, ram)
+		latest_file_path=temp_output_path
+	if s3:
+		temp_output_path=op.join(output_path, "cloud_removal_output_step3.tif")
+		step3(latest_file_path, dem_path, hs_min, hs_max, temp_output_path, ram)
+		latest_file_path=temp_output_path
+	if s4:
+		temp_output_path=op.join(output_path, "cloud_removal_output_step4.tif")
+		step4(latest_file_path, temp_output_path, window_size)
+		latest_file_path=temp_output_path
+	if s5:
+		output_path=op.join(output_path, "cloud_removal_output_step5.tif")
+		step5(latest_file_path, dem_path, output_path, window_size)
+		latest_file_path=temp_output_path
 
 if __name__ == "__main__":
-    if len(sys.argv) != 9:
+    if len(sys.argv) != 2:
         show_help()
     else:
         main(sys.argv)
