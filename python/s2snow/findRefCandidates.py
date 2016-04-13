e19913896d1a2be7b3fba82c74669be8a53e476b
Tristan Klempka <tristan.klempka@cnes.fr>
2016-04-13 16:28:30 +0200

    ENH: add simple script to help finding images with % snow and % cloud

---
+++ python/s2snow/findRefCandidates.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os, sys
+import os.path as op
+from lxml import etree
+
+def main(argv):
+	minsnowthreshold = argv[1]
+	maxsnowthreshold = argv[2]
+	mincloudthreshold = argv[3]
+	maxcloudthreshold = argv[4]
+	
+	total_images = 0
+	
+	for root, dirs, files in os.walk("."):
+		for name in files:
+			if name == "metadata.xml":
+				tree = etree.parse(op.join(root, name))
+				snow_percent = float(tree.find("./Global_Index_List/QUALITY_INDEX/[@name='SnowPercent']").text)
+				cloud_percent = float(tree.find("./Global_Index_List/QUALITY_INDEX/[@name='CloudPercent']").text)
+				
+				# Find potential
+				if snow_percent > minsnowthreshold and cloud_percent > mincloudthreshold and snow_percent < maxsnowthreshold and cloud_percent < maxcloudthreshold :
+					print root
+					print "snow percent: " + str(snow_percent)
+					print "cloud percent: " + str(cloud_percent)
+					total_images += 1
+					
+	print "total images :" + str(total_images)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 5:
+		print "Missing arguments"
+	else:
+		main(sys.argv)
