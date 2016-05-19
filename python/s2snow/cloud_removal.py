BUG: Fix wrong cloud percent computation. Cloud percent is now calculated from the whole cloud mask instead of cloud_refine.tif from LIS

python/s2snow/cloud_removal.py
@@ -46,6 +46,11 @@ def compute_cloudpercent(image_path):
 	tot_pix = np.sum(array_image != 254)
 	return (float(cloud)/float(tot_pix))*100
 
+def compute_cloud(image):
+	array, dataset = get_raster_as_array(image)
+	msk_cloud = (array == 205)
+	return np.sum(msk_cloud)
+
 def step1(m2_path, m1_path, t0_path, p1_path, p2_path, output_path, ram):
 	#S(y,x,t) = 1 if (S(y,x,t-1) = 1 and S(y,x,t+1) = 1) 
 	call(["otbcli_BandMath","-ram", str(ram), "-il", m1_path, t0_path, p1_path, "-out", output_path, "-exp", "im2b1==205?((im1b1==100&&im3b1==100)?100:(im1b1==0&&im3b1==0?0:im2b1)):im2b1"])
@@ -132,11 +137,6 @@ def step4_internal(array, array_dem):
 
 	# Use the mask to set corresponding elements
 	array[1:-1,1:-1][mask] = 100
-
-def compute_cloud(image):
-	array, dataset = get_raster_as_array(image)
-	msk_cloud = (array == 205)
-	return np.sum(msk_cloud)
 
 def compute_stats(image, image_relative, image_reference):
 	array, dataset = get_raster_as_array(image)
@@ -226,7 +226,7 @@ def run(data):
 	p2_path=inputs.get("p2Path")
 	dem_path=inputs.get("demPath")
 	ref_path=inputs.get("refPath","norefpath")
-	parameters=data["parameters"]
+	#parameters=data["parameters"]
 	#hs_min=parameters.get("hsMin")
 	#hs_max=parameters.get("hsMax")
 	#window_size=parameters.get("windowSize")

python/s2snow/format_output.py
@@ -8,6 +8,8 @@ import datetime
 from lxml import etree
 from shutil import copyfile
 import gdal
+import gdalconst
+import numpy as np
 
 # run subprocess and write to stdout and stderr
 def call_subprocess(process_list):
@@ -16,13 +18,32 @@ def call_subprocess(process_list):
     print out
     sys.stderr.write(err)
 
+def get_raster_as_array(raster_file_name):
+	dataset = gdal.Open(raster_file_name, gdalconst.GA_ReadOnly)    
+	wide = dataset.RasterXSize
+	high = dataset.RasterYSize
+	band = dataset.GetRasterBand(1)
+	array = band.ReadAsArray(0, 0, wide, high)
+	return array, dataset
+
+def compute_cloudpercent(image_path):
+	array_image, dataset_image = get_raster_as_array(image_path)
+	cloud = np.sum(array_image == 205)
+	tot_pix = np.sum(array_image != 254)
+	return (float(cloud)/float(tot_pix))*100
+
+def compute_snowpercent(image_path):
+	array_image, dataset_image = get_raster_as_array(image_path)
+	cloud = np.sum(array_image == 100)
+	tot_pix = np.sum(array_image != 254)
+	return (float(cloud)/float(tot_pix))*100
 
 def format_LIS(snow_detector):
     path_img = snow_detector.img
     pout = snow_detector.path_tmp
     zs = snow_detector.zs
-    snow_percent = snow_detector.snow_percent
-    cloud_percent = snow_detector.cloud_percent
+    snow_percent = compute_snowpercent(op.join(pout, "final_mask.tif"))
+    cloud_percent = compute_cloudpercent(op.join(pout, "final_mask.tif"))
     ram = snow_detector.ram
     mode = snow_detector.mode
     nodata_path = snow_detector.nodata_path
@@ -61,7 +82,7 @@ def format_LIS(snow_detector):
         if extension == ".dbf":
             format_SEB_VEC_values(f)
         copyfile(f, op.join(pout, str_seb_vec))
-        
+	
     root = etree.Element("Source_Product")
     etree.SubElement(root, "PRODUCT_ID").text = product_id
     egil = etree.SubElement(root, "Global_Index_List")