f79e674bdc5156a3a4d0ec36140318ebdcd73d2b
Rashad Kanavath <rashad.kanavath@c-s.fr>
2020-02-13 11:41:01 +0100

    ENH: Add LIS.remote.cmake and update comments

--- otb-module.cmake
+++ otb-module.cmake
@@ -1,20 +1,6 @@
 set(DOCUMENTATION "LIS (Let It Snow) OTB Module.")
 
-# otb_module() defines the module dependencies of LISOTBModule.
-# LISOTBModule depends on:
-#   - OTBCommon (base dependency of all modules)
-#   - OTBApplicationEngine (because we build an application in the module, see 'app' folder)
-# 
-# The tests of module LISOTBModule drag additional dependencies:
-#   - OTBTestKernel (needed for any test driver)
-#   - OTBCommandLine (needed to run tests on applications)
-#   - OTBSWIG (needed to run tests with Python bindings)
-#
-# The option ENABLE_SHARED is needed because this module creates a shared
-# library. It generates a header with usefull export macros
-# (LISOTBModuleExport.h), so that other binaries can link to this library.
-
-otb_module(LISOTBModule
+otb_module(LIS
   ENABLE_SHARED
   DEPENDS
     OTBCommon
